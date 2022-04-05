10557a060ee5f42747bb9e6ac4e1b6795b3cb9e1
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-04-05 17:37:11 +0300

    Initial commit

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time:string
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Message
+
+
